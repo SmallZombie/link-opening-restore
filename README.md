c05132bb615aa77bfaab668c7d5549e3a02fb6ba
SmallZombie <SmallZombie@qq.com>
2024-12-19 16:12:07 +0800

    修复 & 删除
    # 更新日志
    - 修复 #1
    - 删除命令："切换功能开关"

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # Obsidian Link Opener
 [English](README.md) | [简体中文](README.zh-CN.md)
 
-This is an Obsidian plugin that restores the link opening method to <kbd>Ctrl</kbd> + <kbd>Left Click</kbd> in edit mode. You can use the '切换功能开关' command to toggle it.
+This is an Obsidian plugin that restores the link opening method to <kbd>Ctrl</kbd> + <kbd>Left Click</kbd> in edit mode.
--- main.ts
+++ main.ts
@@ -1,73 +1,49 @@
 import { Plugin } from 'obsidian';
 
 
-interface LinkOpeningRestorePluginSettings {
-	isEnabled: boolean;
-}
-
-const DEFAULT_SETTINGS: LinkOpeningRestorePluginSettings = {
-	isEnabled: true
-}
-
-
 export default class LinkOpeningRestore extends Plugin {
-	settings: LinkOpeningRestorePluginSettings;
+	static #g_SELECTOR = '.cm-content';
 
 
 	async onload() {
-		await this.loadSettings();
-
-		this.addCommand({
-			id: 'toggle-feature',
-			name: '切换功能开关',
-			callback: () => {
-				this.settings.isEnabled = !this.settings.isEnabled;
-				this.saveSettings();
-
-				// 仅编辑模式
-				const editors = document.querySelectorAll('.cm-editor');
-				editors.forEach(v => {
-					v.removeEventListener('click', this.handleClick);
-					if (this.settings.isEnabled) {
-						v.addEventListener('click', this.handleClick);
-					}
-				});
-			}
-		});
-
 		this.registerEvent(
 			this.app.workspace.on('active-leaf-change', () => {
-				if (!this.settings.isEnabled) return;
-
-				const editors = document.querySelectorAll('.cm-editor');
-				editors.forEach(v => {
-					v.removeEventListener('click', this.handleClick);
-					v.addEventListener('click', this.handleClick);
-				});
+				this.#addListener();
 			})
 		);
+		this.#addListener();
 	}
 
 	onunload() {
-		const editors = document.querySelectorAll('.cm-editor');
-		editors.forEach(editor => {
-			editor.removeEventListener('click', this.handleClick);
-		});
+		this.#removeListener();
 	}
 
 
-	private handleClick = (event: MouseEvent) => {
+	#clickEventHandler = (event: MouseEvent) => {
 		const target = event.target as HTMLElement;
 		if (target.tagName === 'A' && !event.ctrlKey) {
+			event.preventDefault();
 			event.stopPropagation();
 		}
-	};
-
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
 	}
 
-	async saveSettings() {
-		await this.saveData(this.settings);
+	#addListener() {
+		const els = document.querySelectorAll(LinkOpeningRestore.#g_SELECTOR);
+		els.forEach(v => {
+			v.removeEventListener('click', this.#clickEventHandler);
+			v.addEventListener('click', this.#clickEventHandler, {
+				capture: true
+			});
+		});
+		document.addEventListener('click', this.#clickEventHandler);
+	}
+
+	#removeListener() {
+		const els = document.querySelectorAll(LinkOpeningRestore.#g_SELECTOR);
+		els.forEach(editor => {
+			editor.removeEventListener('click', this.#clickEventHandler, {
+				capture: true
+			});
+		});
 	}
 }
